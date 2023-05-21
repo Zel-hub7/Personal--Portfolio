164fb3a82d60370bf0a50a90dd520b0212d378eb
zel-hub7 <zelalemyohannes62@gmail.com>
2023-05-21 12:23:30 -0700

    modify the js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const humBurger = document.getElementsByClassName('ham-container');
-const navItems = document.getElementsByClassName('nav-list');
-const body = document.getElementsByTagName('body')[0];
+const humBurger = document.getElementsByClassName("ham-container");
+const navItems = document.getElementsByClassName("nav-list");
+const body = document.getElementsByTagName("body")[0];
 const ham = document.getElementById("ham");
 const logo = document.getElementById("logo");
 const mobile = document.querySelector(".mobiles-menu");
@@ -8,48 +8,37 @@ const mobile_li = document.getElementById("mobile-class");
 const hidden = document.querySelector("#hidden");
 const main_section = document.querySelector(".main-section");
 const width = window.innerWidth;
-const mediaQuery = window.matchMedia("(max-width: minpx)");
- 
+const mediaQuery = window.matchMedia("(min-width: 768px)");
+
+// mediaQuery.addListener(function() {
+//     if (mediaQuery.matches) {
+//         main_section.style.display = "block";
+//         logo.style.display = "block";
+
+//       // Do something for screens that are smaller than 768 pixels wide
+//     } else {
+//       // Do something for screens that are 768 pixels wide or larger
+//     }
+//   });
 
 mobile.style.display = "none";
 hidden.style.display = "none";
 
-ham.addEventListener("click", function(){
-    if(mobile.style.display == "none") {
-        mobile.style.display = "block";
-        hidden.style.display = "block";
-        ham.style.display = "none";
-        logo.style.display = "none";
-        main_section.style.display = "none";
-    }
-})
+ham.addEventListener("click", function () {
+  mobile.style.display = "block";
+  hidden.style.display = "block";
+  ham.style.display = "none";
+  logo.style.display = "none";
+//   main_section.style.display = "none";
+});
 
-hidden.addEventListener("click", function() {
-    if(mobile.style.display == "block") {
-        mobile.style.display = "none";
-        hidden.style.display = "none";
-        ham.style.display = "block";
-        logo.style.display = "block";
-        main_section.style.display = "block";
-          
-
-    }
-})
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
+hidden.addEventListener("click", function () {
+  mobile.style.display = "none";
+  hidden.style.display = "none";
+  ham.style.display = "block";
+  logo.style.display = "block";
+  main_section.style.display = "block";
+});
 
 // ham.addEventListener("click", function() {
 //     if(mobile.style.display == "none") {
@@ -62,7 +51,6 @@ hidden.addEventListener("click", function() {
 //          hidden.style.display = "none";
 //          ham.style.display = "block";
 
-
 //         })
 
 //     }
@@ -70,43 +58,25 @@ hidden.addEventListener("click", function() {
 
 // mobile.style.display = "block";
 
-
-
-
-
-
 // ham.addEventListener("click", function(){
 //    mobile_li.foreach(function(mobile_li) {
 //     mobile_li.remove();
 //    });
 // });
 
-
-
-
-
-
-
-
 // function navItemsToggler() {
 //     const humBurger = document.getElementsByClassName('ham-container');
 //     const navItems = document.getElementsByClassName('nav-list');
 //     const body = document.getElementsByTagName('body')[0];
 
-
-
-
-
 //     navItems[0].classList.toggle('nav-toggle-show');
 //     navItems[0].classList.remove('text-center');
 //     navItems[0].classList.remove('align-items-center');
-
 
 // }
 // const element = document.getElementById("logo");
 
 // const submit = document.getElementById("ham");
-
 
 // submit.addEventListener("click", function() {
 //   if(element.style.display == "block") {
@@ -115,4 +85,3 @@ hidden.addEventListener("click", function() {
 //     element.style.display = "block";
 //   }
 // })
-  
