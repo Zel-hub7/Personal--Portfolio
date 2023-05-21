546bca412c2e99c44b6636eeeabbcbd254d23be9
zel-hub7 <zelalemyohannes62@gmail.com>
2023-05-21 10:43:14 -0700

    modify your files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,84 @@
+const humBurger = document.getElementsByClassName('ham-container');
+const navItems = document.getElementsByClassName('nav-list');
+const body = document.getElementsByTagName('body')[0];
+const ham = document.getElementById("ham");
+const logo = document.getElementById("logo");
+const mobile = document.querySelector(".mobiles-menu");
+const mobile_li = document.getElementById("mobile-class");
+const hidden = document.querySelector("#hidden")
+
+
+
+mobile.style.display = "none";
+hidden.style.display = "none";
+
+
+ham.addEventListener("click", function(){
+    if(mobile.style.display == "none") {
+        mobile.style.display = "block";
+        hidden.style.display = "block";
+    }
+})
+// hidden.style.display = "block";
+// // mobile.style.display = "none";
+
+hidden.addEventListener("click", function() {
+    if(mobile.style.display == "block") {
+        mobile.style.display = "none";
+    }
+})
+
+
+
+
+
+
+
+
+// ham.addEventListener("click", function() {
+//     if(mobile.style.display == "none") {
+//         mobile.style.display = "block";
+//         ham.style.display = "none";
+//         hidden.style.display = "block";
+//     } else {
+//         hidden.addEventListener("click", function() {
+//          mobile.style.display == "none";
+//          hidden.style.display = "none";
+//          ham.style.display = "block";
+
+
+//         })
+
+//     }
+// })
+
+// mobile.style.display = "block";
+
+
+
+
+
+
+// ham.addEventListener("click", function(){
+//    mobile_li.foreach(function(mobile_li) {
+//     mobile_li.remove();
+//    });
+// });
+
+
+
+
+
+
+
+
 // function navItemsToggler() {
 //     const humBurger = document.getElementsByClassName('ham-container');
 //     const navItems = document.getElementsByClassName('nav-list');
 //     const body = document.getElementsByTagName('body')[0];
+
+
+
 
 
 //     navItems[0].classList.toggle('nav-toggle-show');
@@ -10,5 +87,16 @@
 
 
 // }
+// const element = document.getElementById("logo");
+
+// const submit = document.getElementById("ham");
 
 
+// submit.addEventListener("click", function() {
+//   if(element.style.display == "block") {
+//     element.style.display = "none";
+//   } else {
+//     element.style.display = "block";
+//   }
+// })
+  
