daf306f77bfbe1d8a3f100e28face439dafe5598
zel-hub7 <zelalemyohannes62@gmail.com>
2023-05-21 04:56:15 -0700

    add index.js file to your project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function navItemsToggler() {
+    const humBurger = document.getElementsByClassName('ham-container');
+    const navItems = document.getElementsByClassName('nav-list');
+    const body = document.getElementsByTagName('body')[0];
+
+}
