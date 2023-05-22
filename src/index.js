b580c7a0470fd0a2d2633ba322b48fd75519f991
zel-hub7 <zelalemyohannes62@gmail.com>
2023-05-22 05:53:47 -0700

    modify your js file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
-const humBurger = document.getElementsByClassName("ham-container");
-const navItems = document.getElementsByClassName("nav-list");
-const body = document.getElementsByTagName("body")[0];
-const ham = document.getElementById("ham");
-const logo = document.getElementById("logo");
-const mobile = document.querySelector(".mobiles-menu");
-const mobile_li = document.querySelectorAll("#mobile-class");
-const hidden = document.querySelector("#hidden");
-const main_section = document.querySelector(".main-section");
-const width = window.innerWidth;
-const mediaQuery = window.matchMedia("(min-width: 768px)");
-const active = document.querySelector("active");
+const ham = document.getElementById('ham');
+const logo = document.getElementById('logo');
+const mobile = document.querySelector('.mobiles-menu');
+const hidden = document.querySelector('#hidden');
+const mainSection = document.querySelector('.main-section');
 const links = document.querySelectorAll('li');
-
-
-
-
 
 mobile.classList.add('remove')
 hidden.style.display = "none";
-
-
-
 
 ham.addEventListener("click", function () {
   mobile.style.display = "block";
@@ -34,13 +20,10 @@ hidden.addEventListener("click", function () {
   mobile.style.display = "none";
   hidden.style.display = "none";
   ham.style.display = "block";
-  main_section.style.display = "block";
+  mainSection.style.display = "block";
   logo.classList.remove('remove');
  
-
 });
-
-
 
 links.forEach(link => {
   link.addEventListener('click', () => {
@@ -48,7 +31,6 @@ links.forEach(link => {
     logo.classList.remove('remove');
     hidden.style.display = "none";
     ham.style.display = "block";
-
 
   });
 });
