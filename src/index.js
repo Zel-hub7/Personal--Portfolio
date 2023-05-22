f1de92f3f34b473403bb77721b910539d27f4af6
zel-hub7 <zelalemyohannes62@gmail.com>
2023-05-22 06:30:26 -0700

    mofiy your file

--- src/index.js
+++ src/index.js
@@ -6,31 +6,29 @@ const mainSection = document.querySelector('.main-section');
 const links = document.querySelectorAll('li');
 
 mobile.classList.add('remove');
-hidden.style.display = "none";
+hidden.style.display = 'none';
 
-ham.addEventListener("click", function () {
-  mobile.style.display = "block";
-  hidden.style.display = "block";
-  ham.style.display = "none";
+ham.addEventListener('click', () => {
+  mobile.style.display = 'block';
+  hidden.style.display = 'block';
+  ham.style.display = 'none';
   ham.classList.add('remove');
   logo.classList.add('remove');
 });
 
-hidden.addEventListener("click", function () {
-  mobile.style.display = "none";
-  hidden.style.display = "none";
-  ham.style.display = "block";
-  mainSection.style.display = "block";
+hidden.addEventListener('click', () => {
+  mobile.style.display = 'none';
+  hidden.style.display = 'none';
+  ham.style.display = 'block';
+  mainSection.style.display = 'block';
   logo.classList.remove('remove');
- 
 });
 
-links.forEach(link => {
+links.forEach((link) => {
   link.addEventListener('click', () => {
-    mobile.style.display = "none";
+    mobile.style.display = 'none';
     logo.classList.remove('remove');
-    hidden.style.display = "none";
-    ham.style.display = "block";
-
+    hidden.style.display = 'none';
+    ham.style.display = 'block';
   });
 });
