e922a03ddd5daba6d4c2c4aed6f7d9584cb43164
zel-hub7 <zelalemyohannes62@gmail.com>
2023-05-23 05:37:38 -0700

    Modify files

--- src/index.js
+++ src/index.js
@@ -32,3 +32,110 @@ links.forEach((link) => {
     ham.style.display = 'block';
   });
 });
+
+// The pop Up window
+const bannerProjects = [
+  {
+    Name: 'projectOne',
+    projectTitle: 'Multi-Post Stories',
+    projectDescription:
+      `A daily selection of privately personalized reads; no accounts or
+      sign-ups required. has been the industry's standard dummy text
+      ever since the 1500s, when an unknown printer took a standard
+      dummy text.`,
+    tech: ['CSS', 'HTML', 'Bootstrap', 'Ruby'],
+    img1: 'Img/recent-works place holder.pngImg/recent-works place holder.png',
+    img2: 'Img/Img Placeholder (2).png',
+  },
+];
+
+function mainProject() {
+  const presentProject = bannerProjects[0];
+  const headLineProject =`<div class="please">
+   
+   <div class="multi-note">
+   <h2 class="multi">${presentProject.projectTitle}</h2>
+   <p>${presentProject.projectDescription}</p>
+   <ul class="recent-project-tech works-flex-item">
+      <li class="recent-lists">
+        <a href="#">${presentProject.tech[0]}</a>
+      </li>
+      <li class="recent-lists">
+        <a href="#">${presentProject.tech[1]}</a>
+      </li>
+      <li class="recent-lists">
+        <a href="#">${presentProject.tech[2]}</a>
+      </li>
+      <li class="recent-lists">
+        <a href="#">${presentProject.tech[3]}</a>
+      </li>
+    </ul>
+    
+    <button id=${presentProject.Name} onclick="clickedButton(this.id)" type ="button" class = "works-flex-item see-recent-project" >See project</button>
+   </div>
+  
+
+  </div>
+  ` 
+  return headLineProject;
+}
+
+const projectInfo = [
+  {
+    id: 1,
+    projectName: 'Data Dashboard Healthcare',
+    projectInfo:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/pro_sec.svg',
+  },
+  {
+    id: 2,
+    projectName: 'Data Dashboard Healthcare',
+    projectInfo:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/Data_content_image.svg',
+  },
+  {
+    id: 3,
+    projectName: 'Website Portfolio',
+    projectInfo:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/website.svg',
+  },
+  {
+    id: 4,
+    projectName: 'Profesional Art Printing Data',
+    projectInfo:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/professional.svg',
+  },
+  {
+    id: 5,
+    projectName: 'Data Dashboard Healthcare',
+    projectInfo:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/Data_content_image.svg',
+  },
+  {
+    id: 6,
+    projectName: 'Website Portfolio',
+    projectInfo:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/website.svg',
+  },
+];
+
+
+
+window.addEventListener('load', () => {
+  document.getElementById('multiStories').innerHTML = mainProject();
+  // document.getElementById('popWindow').innerHTML = showPopup();
+  // document.getElementById('grid_contentdesc').innerHTML = showSections().join('');
+});
+  
